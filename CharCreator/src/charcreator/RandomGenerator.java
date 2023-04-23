b007414c636b87fce7d39d2f69a07f1ac91de72d
Noah Taylor <93206722+noahtay15@users.noreply.github.com>
2023-04-22 21:52:40 -0400

    Update RandomGenerator.java

--- CharCreator/src/charcreator/RandomGenerator.java
+++ CharCreator/src/charcreator/RandomGenerator.java
@@ -1,9 +1,5 @@
 package charcreator;
 
-/**
- * interface used for generating height, weight, and age
- * @author Alex
- */
 public interface RandomGenerator {
     //method with race and gender parameter to be used for calculating random numbers
     public int calcRandom(String race, String gender);  
